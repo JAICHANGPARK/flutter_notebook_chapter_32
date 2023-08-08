e04dc9dddf46e61b8628c9c9762ff754add3d232
JAICHANGPARK <aristojeff@gmail.com>
2023-08-08 18:11:33 +0900

    Create marketplace_bag_page.dart

---
+++ lib/ep1601_marketplace_app/view/marketplace_bag_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MarketplaceBagPage extends StatefulWidget {
+  const MarketplaceBagPage({super.key});
+
+  @override
+  State<MarketplaceBagPage> createState() => _MarketplaceBagPageState();
+}
+
+class _MarketplaceBagPageState extends State<MarketplaceBagPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
