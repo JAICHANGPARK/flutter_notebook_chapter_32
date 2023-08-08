ade6099ea28a03b41a74363833f57fb216e897ae
JAICHANGPARK <aristojeff@gmail.com>
2023-08-08 18:16:39 +0900

    Update marketplace_bag_page.dart

--- lib/ep1601_marketplace_app/view/marketplace_bag_page.dart
+++ lib/ep1601_marketplace_app/view/marketplace_bag_page.dart
@@ -11,8 +11,37 @@ class _MarketplaceBagPageState extends State<MarketplaceBagPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back_ios_new,
+                  ),
+                ),
+                Text("My bag(3)"),
+                Spacer(),
+                Text("Select all"),
+                Checkbox(
+                  value: false,
+                  onChanged: (b) {},
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.green[200],
+              child: Row(
+                children: [
+                  Icon(Icons.local_activity),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
