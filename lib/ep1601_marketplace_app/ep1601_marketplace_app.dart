998a6f9ad08269f630eeeb83e7773087b89eef5b
JAICHANGPARK <aristojeff@gmail.com>
2023-08-08 18:07:10 +0900

    Update ep1601_marketplace_app.dart

---
+++ lib/ep1601_marketplace_app/ep1601_marketplace_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MarketplaceApp());
+}
+
+class MarketplaceApp extends StatelessWidget {
+  const MarketplaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
