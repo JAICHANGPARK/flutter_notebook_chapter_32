099389f392233ec8cd809ed28ccc0859195b22b9
JAICHANGPARK <aristojeff@gmail.com>
2023-08-10 17:49:23 +0900

    Update marketplace_checkout_page.dart

--- lib/ep1601_marketplace_app/view/marketplace_checkout_page.dart
+++ lib/ep1601_marketplace_app/view/marketplace_checkout_page.dart
@@ -17,29 +17,35 @@ class _MarketplaceCheckoutPageState extends State<MarketplaceCheckoutPage> {
         elevation: 0,
         title: Text("Checkout"),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            decoration: BoxDecoration(
-              color: Colors.orange[100],
-            ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.info,
-                  color: Colors.orange,
-                ),
-                Expanded(
-                  child: Text(
-                    "Before making an order, make sure the address is "
-                    "correct and matches your current address",
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: Colors.orange[100],
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.info,
+                    color: Colors.orange,
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Before making an order, make sure the address is "
+                      "correct and matches your current address",
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
