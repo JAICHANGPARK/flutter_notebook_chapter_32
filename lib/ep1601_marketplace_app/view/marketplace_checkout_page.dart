d51b4be59bfb943b5f1c0836415085d73ec80fcb
JAICHANGPARK <aristojeff@gmail.com>
2023-08-10 17:48:31 +0900

    Update marketplace_checkout_page.dart

--- lib/ep1601_marketplace_app/view/marketplace_checkout_page.dart
+++ lib/ep1601_marketplace_app/view/marketplace_checkout_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MarketplaceCheckoutPage extends StatefulWidget {
   const MarketplaceCheckoutPage({super.key});
 
@@ -11,6 +10,37 @@ class MarketplaceCheckoutPage extends StatefulWidget {
 class _MarketplaceCheckoutPageState extends State<MarketplaceCheckoutPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text("Checkout"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.orange[100],
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.info,
+                  color: Colors.orange,
+                ),
+                Expanded(
+                  child: Text(
+                    "Before making an order, make sure the address is "
+                    "correct and matches your current address",
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
