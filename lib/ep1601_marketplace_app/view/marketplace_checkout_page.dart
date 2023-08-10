6fc513b4958bc943fb87440ddec8c6780f5eba96
JAICHANGPARK <aristojeff@gmail.com>
2023-08-10 17:45:56 +0900

    Create marketplace_checkout_page.dart

---
+++ lib/ep1601_marketplace_app/view/marketplace_checkout_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MarketplaceCheckoutPage extends StatefulWidget {
+  const MarketplaceCheckoutPage({super.key});
+
+  @override
+  State<MarketplaceCheckoutPage> createState() => _MarketplaceCheckoutPageState();
+}
+
+class _MarketplaceCheckoutPageState extends State<MarketplaceCheckoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
