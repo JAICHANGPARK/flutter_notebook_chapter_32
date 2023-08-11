ba92e90b5febc8ef7e33b989f2a8b6ee427183a4
JAICHANGPARK <aristojeff@gmail.com>
2023-08-11 17:49:30 +0900

    Update ep1604_main.dart

--- lib/ep1604_fashion_store_app/ep1604_main.dart
+++ lib/ep1604_fashion_store_app/ep1604_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_32/ep1604_fashion_store_app/view/fashion_store_main_page.dart';
 
 
 void main() {
@@ -11,6 +12,8 @@ class FashionStoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FashionStoreMainPage(),
+    );
   }
 }
