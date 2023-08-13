260edc02cc420a2a8da897156645b21ee520573d
JAICHANGPARK <aristojeff@gmail.com>
2023-08-13 10:15:54 +0900

    Update fashion_store_detail_page.dart

--- lib/ep1604_fashion_store_app/view/fashion_store_detail_page.dart
+++ lib/ep1604_fashion_store_app/view/fashion_store_detail_page.dart
@@ -16,9 +16,49 @@ class _FashionStoreDetailPageState extends State<FashionStoreDetailPage> {
           children: [
             Row(
               children: [
-
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.clear,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: const Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.search),
+                          hintText: "What's the lowdown, pal?",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: Center(
+                    child: Badge(
+                      label: const Text("99"),
+                      child: IconButton(
+                        icon: const Icon(Icons.notifications_active_outlined),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
