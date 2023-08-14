02b61e95f94e3e26e55ce52739db629f9c7c30ac
JAICHANGPARK <aristojeff@gmail.com>
2023-08-14 18:27:11 +0900

    Create fashion_store_bag_page.dart

---
+++ lib/ep1604_fashion_store_app/view/fashion_store_bag_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FashionStoreBagPage extends StatefulWidget {
+  const FashionStoreBagPage({super.key});
+
+  @override
+  State<FashionStoreBagPage> createState() => _FashionStoreBagPageState();
+}
+
+class _FashionStoreBagPageState extends State<FashionStoreBagPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
