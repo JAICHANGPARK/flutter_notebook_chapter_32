5ce1ca870fcfbb743b8ad6d4ec62c7be5b61478b
JAICHANGPARK <aristojeff@gmail.com>
2023-08-15 08:57:59 +0900

    Update fashion_store_bag_page.dart

--- lib/ep1604_fashion_store_app/view/fashion_store_bag_page.dart
+++ lib/ep1604_fashion_store_app/view/fashion_store_bag_page.dart
@@ -97,7 +97,7 @@ class _FashionStoreBagPageState extends State<FashionStoreBagPage> {
                 Container(
                   height: 160,
                   decoration: BoxDecoration(
-                      color: Colors.blue,
+
                       border: Border(
                         top: BorderSide(),
                       )),
@@ -182,7 +182,97 @@ class _FashionStoreBagPageState extends State<FashionStoreBagPage> {
                       )
                     ],
                   ),
-                )
+                ),
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+
+                      border: Border(
+                        top: BorderSide(),
+                      )),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 140,
+                        decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(),
+                              left: BorderSide(),
+                            )),
+                        child: Column(
+                          children: [
+                            Expanded(child: Container()),
+                            const Divider(
+                              color: Colors.black,
+                              height: 0,
+                            ),
+                            const IntrinsicHeight(
+                              child: Row(
+                                children: [
+                                  Icon(Icons.remove),
+                                  VerticalDivider(
+                                    color: Colors.black,
+                                  ),
+                                  Expanded(
+                                    child: Center(
+                                      child: Text("1"),
+                                    ),
+                                  ),
+                                  VerticalDivider(
+                                    color: Colors.black,
+                                  ),
+                                  Icon(Icons.add)
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Text(
+                                "BIG BASIC SHORT",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const Text(
+                                "SHIPPED NOVEMBER 12TH",
+                              ),
+                              const Text("SIZE M"),
+                              Row(
+                                children: [
+                                  const Text("\$ 999"),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  const Text("\$ 666"),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(),
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                                    child: const Text("- 23%"),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Divider(height: 0,color: Colors.black,)
               ],
             )),
             Container(
