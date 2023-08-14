e994e539885e9b4e970ba37eaf7dd6bda60d8815
JAICHANGPARK <aristojeff@gmail.com>
2023-08-14 18:29:50 +0900

    Update fashion_store_bag_page.dart

--- lib/ep1604_fashion_store_app/view/fashion_store_bag_page.dart
+++ lib/ep1604_fashion_store_app/view/fashion_store_bag_page.dart
@@ -14,7 +14,69 @@ class _FashionStoreBagPageState extends State<FashionStoreBagPage> {
       body: SafeArea(
         child: Column(
           children: [
-
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.clear,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    height: 50,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: const Center(
+                      child: Text(
+                        "SHOP BAG",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: Center(
+                    child: IconButton(
+                      icon: const Icon(Icons.more_horiz),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 64,
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 32,
+                    width: 32,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(),
+                    ),
+                  ),
+                  Text("SHIP TO DREAM"),
+                  Spacer(),
+                  Text("SEOUL, KOREA"),
+                  Icon(Icons.keyboard_arrow_down),
+                ],
+              ),
+            )
           ],
         ),
       ),
