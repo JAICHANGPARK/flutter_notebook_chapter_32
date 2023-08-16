f3b0496a7a04565fd2f5a06223cb5c29a8835d17
JAICHANGPARK <aristojeff@gmail.com>
2023-08-16 18:56:57 +0900

    Update ep1609_main.dart

--- lib/ep1609_pet_app/ep1609_main.dart
+++ lib/ep1609_pet_app/ep1609_main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+
+}
+
+class FetApp extends StatelessWidget {
+  const FetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
