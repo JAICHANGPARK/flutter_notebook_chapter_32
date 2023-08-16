4d4918fdda26167c2b867e8145586d4236352287
JAICHANGPARK <aristojeff@gmail.com>
2023-08-16 19:01:01 +0900

    Update pet_home_page.dart

--- lib/ep1609_pet_app/views/pet_home_page.dart
+++ lib/ep1609_pet_app/views/pet_home_page.dart
@@ -11,6 +11,10 @@ class _PetHomePageState extends State<PetHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("My Pets"),
+        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.add_circle_outlined)],
+      ),
       body: SafeArea(
         child: Column(
           children: [
