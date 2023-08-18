9752f379ed3e097eeb8e0e835a1574c65bcd8384
JAICHANGPARK <aristojeff@gmail.com>
2023-08-18 18:33:12 +0900

    Update meseum_ticket_main_page.dart

--- lib/ep1611_museum_ticket_app/view/meseum_ticket_main_page.dart
+++ lib/ep1611_museum_ticket_app/view/meseum_ticket_main_page.dart
@@ -10,6 +10,36 @@ class MuseumTicketMainPage extends StatefulWidget {
 class _MuseumTicketMainPageState extends State<MuseumTicketMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.confirmation_number),
+            label: "Tickets",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Tickets",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.museum),
+            label: "Tickets",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: "Tickets",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
   }
 }
