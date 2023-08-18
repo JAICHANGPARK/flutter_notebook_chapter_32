f90254018151b438586750c499d7fc693857304f
JAICHANGPARK <aristojeff@gmail.com>
2023-08-18 18:30:35 +0900

    Update meseum_ticket_main_page.dart

---
+++ lib/ep1611_museum_ticket_app/view/meseum_ticket_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MuseumTicketMainPage extends StatefulWidget {
+  const MuseumTicketMainPage({super.key});
+
+  @override
+  State<MuseumTicketMainPage> createState() => _MuseumTicketMainPageState();
+}
+
+class _MuseumTicketMainPageState extends State<MuseumTicketMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
