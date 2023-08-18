22cb7b319363c35f675318e3d97c490b49ded2e7
JAICHANGPARK <aristojeff@gmail.com>
2023-08-18 18:29:32 +0900

    Create ep1611_main.dart

---
+++ lib/ep1611_museum_ticket_app/ep1611_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MuseumTicketApp extends StatelessWidget {
+  const MuseumTicketApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
