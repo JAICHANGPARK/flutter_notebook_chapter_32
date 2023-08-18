ddb3491dc1598fc509f939b937ee0c3142c02cda
JAICHANGPARK <aristojeff@gmail.com>
2023-08-18 18:34:49 +0900

    Update ep1611_main.dart

--- lib/ep1611_museum_ticket_app/ep1611_main.dart
+++ lib/ep1611_museum_ticket_app/ep1611_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_32/ep1611_museum_ticket_app/view/meseum_ticket_main_page.dart';
 
 void main() {
   runApp(MuseumTicketApp());
@@ -9,6 +10,8 @@ class MuseumTicketApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MuseumTicketMainPage(),
+    );
   }
 }
