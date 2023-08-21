34f86922779300061e0b5c9649b5908904906b70
JAICHANGPARK <aristojeff@gmail.com>
2023-08-21 17:22:41 +0900

    Create social_medial_main_page.dart

---
+++ lib/ep1614_social_medial_app/view/social_medial_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SocialMediaMainPage extends StatefulWidget {
+  const SocialMediaMainPage({super.key});
+
+  @override
+  State<SocialMediaMainPage> createState() => _SocialMediaMainPageState();
+}
+
+class _SocialMediaMainPageState extends State<SocialMediaMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
