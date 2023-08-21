c32b7b06a75e24588e2770d3a6b812f5ba65e32c
JAICHANGPARK <aristojeff@gmail.com>
2023-08-21 17:22:04 +0900

    Update ep1614_main.dart

--- lib/ep1614_social_medial_app/ep1614_main.dart
+++ lib/ep1614_social_medial_app/ep1614_main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class SocialMediaApp extends StatelessWidget {
+  const SocialMediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
