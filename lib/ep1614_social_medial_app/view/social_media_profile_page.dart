47530a61d6608896ec507dc159f6376f77e4efdc
JAICHANGPARK <aristojeff@gmail.com>
2023-08-24 19:09:40 +0900

    Update social_media_profile_page.dart

---
+++ lib/ep1614_social_medial_app/view/social_media_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaProfilePage extends StatefulWidget {
+  const SocialMediaProfilePage({super.key});
+
+  @override
+  State<SocialMediaProfilePage> createState() => _SocialMediaProfilePageState();
+}
+
+class _SocialMediaProfilePageState extends State<SocialMediaProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
