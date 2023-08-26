5121a26e0099b6ba36a3706f278ebd85212c5a75
JAICHANGPARK <aristojeff@gmail.com>
2023-08-26 09:11:57 +0900

    Create ep1619_main.dart

---
+++ lib/ep1619_tourist_app/ep1619_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TouristApp extends StatelessWidget {
+  const TouristApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
