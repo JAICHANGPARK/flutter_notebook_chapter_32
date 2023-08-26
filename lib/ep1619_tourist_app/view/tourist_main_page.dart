0858dfda3f54c9dd7d5e07596be1989c33ecd1af
JAICHANGPARK <aristojeff@gmail.com>
2023-08-26 09:14:01 +0900

    Update tourist_main_page.dart

---
+++ lib/ep1619_tourist_app/view/tourist_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TouristMainPage extends StatefulWidget {
+  const TouristMainPage({super.key});
+
+  @override
+  State<TouristMainPage> createState() => _TouristMainPageState();
+}
+
+class _TouristMainPageState extends State<TouristMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
