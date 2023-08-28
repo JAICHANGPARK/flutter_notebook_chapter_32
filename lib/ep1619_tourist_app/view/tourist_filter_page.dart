3e2f04dbd63da62eb99a5e2c030a38bb7d2c571c
JAICHANGPARK <aristojeff@gmail.com>
2023-08-28 21:09:20 +0900

    Create tourist_filter_page.dart

---
+++ lib/ep1619_tourist_app/view/tourist_filter_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TouristFilterPage extends StatefulWidget {
+  const TouristFilterPage({super.key});
+
+  @override
+  State<TouristFilterPage> createState() => _TouristFilterPageState();
+}
+
+class _TouristFilterPageState extends State<TouristFilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
