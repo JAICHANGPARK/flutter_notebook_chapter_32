d2e864e194c9ed85441e6f0712cef4874ecd65f8
JAICHANGPARK <aristojeff@gmail.com>
2023-08-28 21:15:24 +0900

    Update tourist_filter_page.dart

--- lib/ep1619_tourist_app/view/tourist_filter_page.dart
+++ lib/ep1619_tourist_app/view/tourist_filter_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TouristFilterPage extends StatefulWidget {
   const TouristFilterPage({super.key});
 
@@ -17,6 +16,34 @@ class _TouristFilterPageState extends State<TouristFilterPage> {
         backgroundColor: Colors.white,
         elevation: 0,
         foregroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Hotel, Flight, Place, Food..",
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.location_on_outlined,
+                  ), onPressed: () {  },
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
