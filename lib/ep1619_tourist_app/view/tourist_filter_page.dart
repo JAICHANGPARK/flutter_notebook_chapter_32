193612ae861996b2b99b4bcbadb8c7dae93fa681
JAICHANGPARK <aristojeff@gmail.com>
2023-08-28 21:17:11 +0900

    Update tourist_filter_page.dart

--- lib/ep1619_tourist_app/view/tourist_filter_page.dart
+++ lib/ep1619_tourist_app/view/tourist_filter_page.dart
@@ -19,29 +19,55 @@ class _TouristFilterPageState extends State<TouristFilterPage> {
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Hotel, Flight, Place, Food..",
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Hotel, Flight, Place, Food..",
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.location_on_outlined,
-                  ), onPressed: () {  },
+                SizedBox(
+                  width: 16,
                 ),
-              )
-            ],
+                Container(
+                  decoration: BoxDecoration(),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.location_on_outlined,
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text("Sort Result"),
+          Container(
+            height: 48,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          Text("Rating"),
+          Container(
+            height: 48,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          Text("Price Range"),
+          RangeSlider(
+            values: RangeValues(0.1, 0.5),
+            onChanged: (d) {},
           )
         ],
       ),
