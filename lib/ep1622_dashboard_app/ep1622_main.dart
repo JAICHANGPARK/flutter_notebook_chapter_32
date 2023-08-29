529ed9db1b2baf9e8ae65a81f11c71a58f060ecf
JAICHANGPARK <aristojeff@gmail.com>
2023-08-29 20:12:40 +0900

    Update ep1622_main.dart

--- lib/ep1622_dashboard_app/ep1622_main.dart
+++ lib/ep1622_dashboard_app/ep1622_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_32/ep1622_dashboard_app/view/dashboard_main_page.dart';
 
 void main() {
-
+  runApp(DashboardApp());
 }
 
 class DashboardApp extends StatelessWidget {
