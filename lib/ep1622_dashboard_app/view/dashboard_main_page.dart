75dd7f5f8ba45fd919c4114ccdea291620e33ea4
JAICHANGPARK <aristojeff@gmail.com>
2023-08-29 20:14:45 +0900

    Update dashboard_main_page.dart

--- lib/ep1622_dashboard_app/view/dashboard_main_page.dart
+++ lib/ep1622_dashboard_app/view/dashboard_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class DashboardMainPage extends StatefulWidget {
   const DashboardMainPage({super.key});
 
@@ -11,7 +10,29 @@ class DashboardMainPage extends StatefulWidget {
 class _DashboardMainPageState extends State<DashboardMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {},
+                  ),
+                ),
+                Text(
+                  "Product Oprimz",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
