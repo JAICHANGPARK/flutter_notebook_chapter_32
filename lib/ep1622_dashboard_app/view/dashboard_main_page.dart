63fb868e7ac5c94f54590c496f66c6d0e2ebe690
JAICHANGPARK <aristojeff@gmail.com>
2023-08-29 20:12:13 +0900

    Create dashboard_main_page.dart

---
+++ lib/ep1622_dashboard_app/view/dashboard_main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class DashboardMainPage extends StatefulWidget {
+  const DashboardMainPage({super.key});
+
+  @override
+  State<DashboardMainPage> createState() => _DashboardMainPageState();
+}
+
+class _DashboardMainPageState extends State<DashboardMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
