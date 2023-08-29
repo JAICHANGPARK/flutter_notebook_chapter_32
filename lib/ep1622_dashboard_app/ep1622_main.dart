2e945001c77aaa75ef0100798101481a93d1c663
JAICHANGPARK <aristojeff@gmail.com>
2023-08-29 20:11:17 +0900

    Create ep1622_main.dart

---
+++ lib/ep1622_dashboard_app/ep1622_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class DashboardApp extends StatelessWidget {
+  const DashboardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
