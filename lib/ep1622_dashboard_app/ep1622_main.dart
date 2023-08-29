1bae612e1b97acad173fe0092ae3cc63a188f50f
JAICHANGPARK <aristojeff@gmail.com>
2023-08-29 20:12:26 +0900

    Update ep1622_main.dart

--- lib/ep1622_dashboard_app/ep1622_main.dart
+++ lib/ep1622_dashboard_app/ep1622_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_32/ep1622_dashboard_app/view/dashboard_main_page.dart';
 
 void main() {
 
@@ -9,6 +10,8 @@ class DashboardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: DashboardMainPage(),
+    );
   }
 }
