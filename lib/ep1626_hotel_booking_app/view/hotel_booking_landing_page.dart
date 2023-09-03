c0d98dffa6cf20e2131332e5c716d5aa0b27ac2e
JAICHANGPARK <aristojeff@gmail.com>
2023-09-03 09:43:50 +0900

    Update hotel_booking_landing_page.dart

--- lib/ep1626_hotel_booking_app/view/hotel_booking_landing_page.dart
+++ lib/ep1626_hotel_booking_app/view/hotel_booking_landing_page.dart
@@ -8,7 +8,12 @@ class HotelBookingLandingPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: Image.network("")),
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
     );
