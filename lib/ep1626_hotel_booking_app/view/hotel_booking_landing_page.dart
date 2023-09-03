b49770131b6ac15e334087f61d3496e5119f127a
JAICHANGPARK <aristojeff@gmail.com>
2023-09-03 09:40:58 +0900

    Update hotel_booking_landing_page.dart

--- lib/ep1626_hotel_booking_app/view/hotel_booking_landing_page.dart
+++ lib/ep1626_hotel_booking_app/view/hotel_booking_landing_page.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class HotelBookingLandingPage extends StatelessWidget {
+  const HotelBookingLandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
