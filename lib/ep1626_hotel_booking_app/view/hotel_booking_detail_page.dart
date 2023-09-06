815873d1a4381a1a6531a8d183b4463e6ca8784d
JAICHANGPARK <aristojeff@gmail.com>
2023-09-06 21:29:40 +0900

    Update hotel_booking_detail_page.dart

--- lib/ep1626_hotel_booking_app/view/hotel_booking_detail_page.dart
+++ lib/ep1626_hotel_booking_app/view/hotel_booking_detail_page.dart
@@ -5,6 +5,17 @@ class HotelBookingDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2017/08/01/23/51/apple-2568755_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
