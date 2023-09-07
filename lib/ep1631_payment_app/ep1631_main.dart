857f9d4115698638267cf5148fdcc19d947f2bc5
JAICHANGPARK <aristojeff@gmail.com>
2023-09-07 18:35:19 +0900

    Update ep1631_main.dart

--- lib/ep1631_payment_app/ep1631_main.dart
+++ lib/ep1631_payment_app/ep1631_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/payment_app_home_page.dart';
+
 void main() {
   runApp(PaymentApp());
 }
@@ -10,7 +12,7 @@ class PaymentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+    home: PaymentAppHomePage(),
     );
   }
 }
