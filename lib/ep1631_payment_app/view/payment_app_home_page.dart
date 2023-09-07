8595e04b6e0ebc9fcaf333c9c99ab1455a86bc1a
JAICHANGPARK <aristojeff@gmail.com>
2023-09-07 18:34:33 +0900

    Update payment_app_home_page.dart

---
+++ lib/ep1631_payment_app/view/payment_app_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaymentAppHomePage extends StatelessWidget {
+  const PaymentAppHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
