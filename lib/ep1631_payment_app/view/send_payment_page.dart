975e86ab56901e4336a944a3876d4c060032e03f
JAICHANGPARK <aristojeff@gmail.com>
2023-09-09 08:45:05 +0900

    Create send_payment_page.dart

---
+++ lib/ep1631_payment_app/view/send_payment_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SendPaymentPage extends StatefulWidget {
+  const SendPaymentPage({super.key});
+
+  @override
+  State<SendPaymentPage> createState() => _SendPaymentPageState();
+}
+
+class _SendPaymentPageState extends State<SendPaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
