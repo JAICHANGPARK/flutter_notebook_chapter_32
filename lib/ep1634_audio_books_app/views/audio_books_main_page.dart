94e7d97f1d7ca5384b2693adf92f842cf71e46a4
JAICHANGPARK <aristojeff@gmail.com>
2023-09-10 09:52:01 +0900

    Create audio_books_main_page.dart

---
+++ lib/ep1634_audio_books_app/views/audio_books_main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AudioBooksMainPage extends StatefulWidget {
+  const AudioBooksMainPage({super.key});
+
+  @override
+  State<AudioBooksMainPage> createState() => _AudioBooksMainPageState();
+}
+
+class _AudioBooksMainPageState extends State<AudioBooksMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
