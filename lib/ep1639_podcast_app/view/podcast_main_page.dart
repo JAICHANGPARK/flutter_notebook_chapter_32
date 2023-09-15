8a8b4d52842c9fcbc044c7ad4c1c099db2f775ca
JAICHANGPARK <aristojeff@gmail.com>
2023-09-15 17:33:33 +0900

    Create podcast_main_page.dart

---
+++ lib/ep1639_podcast_app/view/podcast_main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PodcastMainPage extends StatelessWidget {
+  const PodcastMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
