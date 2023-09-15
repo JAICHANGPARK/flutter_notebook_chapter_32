cb027177fefd01403893298e9168a029ad194153
JAICHANGPARK <aristojeff@gmail.com>
2023-09-15 17:32:12 +0900

    Create ep1639_main.dart

---
+++ lib/ep1639_podcast_app/ep1639_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PodcastApp());
+}
+
+class PodcastApp extends StatelessWidget {
+  const PodcastApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
