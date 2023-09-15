ad6d4b278c6cdb3e1d0c1ba276a2f298fdfd66d6
JAICHANGPARK <aristojeff@gmail.com>
2023-09-15 17:37:24 +0900

    Update podcast_main_page.dart

--- lib/ep1639_podcast_app/view/podcast_main_page.dart
+++ lib/ep1639_podcast_app/view/podcast_main_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
-
 class PodcastMainPage extends StatelessWidget {
   const PodcastMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cell_tower),
+            label: "Live",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          )
+        ],
+      ),
+    );
   }
 }
