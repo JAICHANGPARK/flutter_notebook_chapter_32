914d3b1f57267dac54055c3b7f515c064cfba791
JAICHANGPARK <aristojeff@gmail.com>
2023-09-17 09:46:09 +0900

    Update messaging_main_page.dart

--- lib/ep1641_messaging_app/views/messaging_main_page.dart
+++ lib/ep1641_messaging_app/views/messaging_main_page.dart
@@ -15,8 +15,44 @@ class _MessagingMainPageState extends State<MessagingMainPage> {
       body: Column(
         children: [
           Container(
-            height: 100,
-          )
+            height: 150,
+            padding: EdgeInsets.only(top: 48, left: 16, right: 16),
+            color: Colors.white,
+            child: Row(
+              children: [
+                Text(
+                  "Chat",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[200]!,
+                      ),
+                    ),
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(),
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.deepOrange,
+                  radius: 24,
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
         ],
       ),
     );
