6acac782882e6d80f2a8128677a5f1ffa1392b41
JAICHANGPARK <aristojeff@gmail.com>
2023-09-17 09:40:08 +0900

    Create messaging_main_page.dart

---
+++ lib/ep1641_messaging_app/views/messaging_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MessagingMainPage extends StatefulWidget {
+  const MessagingMainPage({super.key});
+
+  @override
+  State<MessagingMainPage> createState() => _MessagingMainPageState();
+}
+
+class _MessagingMainPageState extends State<MessagingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
