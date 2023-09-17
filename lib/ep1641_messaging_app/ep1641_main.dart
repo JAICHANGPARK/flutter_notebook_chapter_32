f451a49b09b7834227337535f9badadddc80f08b
JAICHANGPARK <aristojeff@gmail.com>
2023-09-17 09:39:20 +0900

    Create ep1641_main.dart

---
+++ lib/ep1641_messaging_app/ep1641_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class MessagingApp extends StatelessWidget {
+  const MessagingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
