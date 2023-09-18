37b1d65198def5e2241a5d6a2cf47e6c8b711822
JAICHANGPARK <aristojeff@gmail.com>
2023-09-18 16:58:19 +0900

    Update chat_page.dart

--- lib/ep1641_messaging_app/views/chat_page.dart
+++ lib/ep1641_messaging_app/views/chat_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
@@ -12,9 +11,29 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: Column(
         children: [
-
+          Container(
+            height: 150,
+            padding: const EdgeInsets.only(top: 48, left: 24, right: 24),
+            color: Colors.white,
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 24,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("Dream Walker"),
+                      Text("Online"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
