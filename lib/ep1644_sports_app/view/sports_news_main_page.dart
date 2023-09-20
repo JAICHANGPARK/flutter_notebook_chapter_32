a838859a79671b2738ddbf7d78028ef0ad020ad1
JAICHANGPARK <aristojeff@gmail.com>
2023-09-20 18:30:51 +0900

    Update sports_news_main_page.dart

--- lib/ep1644_sports_app/view/sports_news_main_page.dart
+++ lib/ep1644_sports_app/view/sports_news_main_page.dart
@@ -1,2 +1,24 @@
 import 'package:flutter/material.dart';
 
+class SportsNewsMainPage extends StatefulWidget {
+  const SportsNewsMainPage({super.key});
+
+  @override
+  State<SportsNewsMainPage> createState() => _SportsNewsMainPageState();
+}
+
+class _SportsNewsMainPageState extends State<SportsNewsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
+  }
+}
