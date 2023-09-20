20bd77d94509bb47b8e7c451236f161d83e493d5
JAICHANGPARK <aristojeff@gmail.com>
2023-09-20 18:27:56 +0900

    Update ep1644_main.dart

--- lib/ep1644_sports_app/ep1644_main.dart
+++ lib/ep1644_sports_app/ep1644_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(SportNewsApp());
+}
 
 class SportNewsApp extends StatelessWidget {
   const SportNewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
