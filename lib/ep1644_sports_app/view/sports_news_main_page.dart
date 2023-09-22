609dd38854c8508b08ca8dcb843ed80924e0e9d8
JAICHANGPARK <aristojeff@gmail.com>
2023-09-22 13:58:50 +0900

    Update sports_news_main_page.dart

--- lib/ep1644_sports_app/view/sports_news_main_page.dart
+++ lib/ep1644_sports_app/view/sports_news_main_page.dart
@@ -200,9 +200,22 @@ class _SportsNewsMainPageState extends State<SportsNewsMainPage> {
                             SizedBox(
                               width: 8,
                             ),
-                            Text("Skysport"),
+                            Text(
+                              "Skysport",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                             Spacer(),
-                            Text("7m Ago"),
+                            Text(
+                              "7m Ago",
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
                             Icon(Icons.more_horiz),
                           ],
                         ),
@@ -218,7 +231,9 @@ class _SportsNewsMainPageState extends State<SportsNewsMainPage> {
                             fontSize: 18,
                           ),
                         ),
-                        SizedBox(height: 8,),
+                        SizedBox(
+                          height: 8,
+                        ),
                         Text(
                           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                           maxLines: 2,
