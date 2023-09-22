71ae4e1ed82f072319267c2d1f858375f29cb761
JAICHANGPARK <aristojeff@gmail.com>
2023-09-22 13:56:36 +0900

    Update sports_news_main_page.dart

--- lib/ep1644_sports_app/view/sports_news_main_page.dart
+++ lib/ep1644_sports_app/view/sports_news_main_page.dart
@@ -192,10 +192,11 @@ class _SportsNewsMainPageState extends State<SportsNewsMainPage> {
                         Row(
                           children: [
                             Container(
-                              height: 28,
-                              width: 28,
+                              height: 32,
+                              width: 32,
                               color: Colors.blue,
                             ),
+                            SizedBox(width: 8,),
                             Text("Skysport"),
                             Spacer(),
                             Text("7m Ago"),
@@ -203,11 +204,15 @@ class _SportsNewsMainPageState extends State<SportsNewsMainPage> {
                           ],
                         ),
                         Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blue
-                          ),
+                          decoration: BoxDecoration(color: Colors.blue),
                           height: 240,
                         ),
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, "),
+                        Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        )
                       ],
                     ),
                   );
