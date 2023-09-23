96cb49c14b2140f210baac9fc059f1d20a8374b0
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 08:45:44 +0900

    Create sports_news_today_widget.dart

---
+++ lib/ep1644_sports_app/view/sports_news_today_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SportsNewsTodayWidget extends StatefulWidget {
+  const SportsNewsTodayWidget({super.key});
+
+  @override
+  State<SportsNewsTodayWidget> createState() => _SportsNewsTodayWidgetState();
+}
+
+class _SportsNewsTodayWidgetState extends State<SportsNewsTodayWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
