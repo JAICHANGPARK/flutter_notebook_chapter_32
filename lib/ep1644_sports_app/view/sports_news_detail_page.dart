5aac91b89f29b6171c9f5414832dcd348f89bcf6
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 09:17:42 +0900

    Update sports_news_detail_page.dart

--- lib/ep1644_sports_app/view/sports_news_detail_page.dart
+++ lib/ep1644_sports_app/view/sports_news_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SportsNewsDetailPage extends StatefulWidget {
   const SportsNewsDetailPage({super.key});
 
@@ -15,7 +14,33 @@ class _SportsNewsDetailPageState extends State<SportsNewsDetailPage> {
       body: SafeArea(
         child: Column(
           children: [
-            
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                ),
+                // SizedBox(width: 16,)
+                Container(
+                  height: 36,
+                  width: 36,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                SizedBox(width: 4,),
+                Text("Skysport"),
+
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
