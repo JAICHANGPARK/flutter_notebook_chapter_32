5fcbf72af8384aa0161ac270c924beb0344aa24a
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 09:32:33 +0900

    Update sports_news_detail_page.dart

--- lib/ep1644_sports_app/view/sports_news_detail_page.dart
+++ lib/ep1644_sports_app/view/sports_news_detail_page.dart
@@ -154,7 +154,9 @@ class _SportsNewsDetailPageState extends State<SportsNewsDetailPage> {
                     const Divider(
                       color: Colors.grey,
                     ),
+                    SizedBox(height: 8,),
                     Text("Tuesday 19 September 2023 13:36 UK"),
+                    SizedBox(height: 8,),
                     Text(
                       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
                       style: TextStyle(
@@ -162,6 +164,7 @@ class _SportsNewsDetailPageState extends State<SportsNewsDetailPage> {
                         fontSize: 24,
                       ),
                     ),
+                    SizedBox(height: 16,),
                     Text(
                         "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                             "\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
