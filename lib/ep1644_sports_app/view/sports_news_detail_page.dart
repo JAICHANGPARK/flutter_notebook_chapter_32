e09dd505f562ed4b1b8fc8e515f9f07e6eca81fc
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 09:12:52 +0900

    Create sports_news_detail_page.dart

---
+++ lib/ep1644_sports_app/view/sports_news_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SportsNewsDetailPage extends StatefulWidget {
+  const SportsNewsDetailPage({super.key});
+
+  @override
+  State<SportsNewsDetailPage> createState() => _SportsNewsDetailPageState();
+}
+
+class _SportsNewsDetailPageState extends State<SportsNewsDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
